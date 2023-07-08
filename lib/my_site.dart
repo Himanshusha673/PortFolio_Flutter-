196ce20bbdfd24499d3de798c7334077761c212a
himanshusha673 <himanshusaha673@gmail.com>
2023-07-08 16:50:08 +0530

    added to firebase for hosting

--- lib/my_site.dart
+++ lib/my_site.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mysite/core/configs/configs.dart';
 import 'package:mysite/core/configs/connection/bloc/connected_bloc.dart';
@@ -10,32 +11,48 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:sizer/sizer.dart';
 
 class MySite extends StatelessWidget {
-  const MySite({Key? key}) : super(key: key);
+  MySite({Key? key}) : super(key: key);
+
+  final Future<FirebaseApp> _initilization = Firebase.initializeApp();
 
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider<ThemeCubit>(create: (_) => ThemeCubit()),
-        BlocProvider<ConnectedBloc>(create: (context) => ConnectedBloc()),
-      ],
-      child: MultiProvider(
-        providers: [
-          ChangeNotifierProvider(create: (_) => DrawerProvider()),
-          ChangeNotifierProvider(create: (_) => ScrollProvider()),
-        ],
-        child: BlocBuilder<ThemeCubit, ThemeState>(builder: (context, state) {
-          return Sizer(builder: (context, orientation, deviceType) {
-            return MaterialApp(
-              debugShowCheckedModeBanner: false,
-              title: 'Himanshu',
-              theme: AppTheme.themeData(state.isDarkThemeOn, context),
-              initialRoute: "/",
-              routes: {"/": (context) => const NChecking()},
+    return FutureBuilder(
+        future: _initilization,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            print('Error');
+          }
+          if (snapshot.connectionState == ConnectionState.done) {
+            print('done');
+            return MultiBlocProvider(
+              providers: [
+                BlocProvider<ThemeCubit>(create: (_) => ThemeCubit()),
+                BlocProvider<ConnectedBloc>(
+                    create: (context) => ConnectedBloc()),
+              ],
+              child: MultiProvider(
+                providers: [
+                  ChangeNotifierProvider(create: (_) => DrawerProvider()),
+                  ChangeNotifierProvider(create: (_) => ScrollProvider()),
+                ],
+                child: BlocBuilder<ThemeCubit, ThemeState>(
+                    builder: (context, state) {
+                  return Sizer(builder: (context, orientation, deviceType) {
+                    return MaterialApp(
+                      debugShowCheckedModeBanner: false,
+                      title: 'Himanshu',
+                      theme: AppTheme.themeData(state.isDarkThemeOn, context),
+                      initialRoute: "/",
+                      routes: {"/": (context) => const NChecking()},
+                    );
+                  });
+                }),
+              ),
             );
-          });
-        }),
-      ),
-    );
+          }
+
+          return const Center(child: CircularProgressIndicator());
+        });
   }
 }
