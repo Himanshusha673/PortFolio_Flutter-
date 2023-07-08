b4ba3cc2ef677e1711ff176419ff791cb26f8b96
himanshusha673 <himanshusaha673@gmail.com>
2023-07-08 15:19:23 +0530

    adding this portfolio website using Flutter

---
+++ lib/app/utils/services_utils.dart
@@ -0,0 +1,59 @@
+class ServicesUtils {
+  final String name;
+  final String icon;
+  final List<String> tool;
+  final String description;
+
+  ServicesUtils(
+      {required this.name,
+      required this.icon,
+      required this.description,
+      required this.tool});
+}
+
+List<ServicesUtils> servicesUtils = [
+  ServicesUtils(
+    name: 'Android App Development',
+    icon: 'assets/icons/android.svg',
+    description:
+        "Are you interested in the great Mobile app? Let's make it a reality.",
+    tool: [
+      'Flutter',
+      'React Native',
+    ],
+  ),
+  ServicesUtils(
+    name: 'iOS App Development',
+    icon: 'assets/icons/apple.svg',
+    description:
+        "Are you interested in the great Mobile app? Let's make it a reality.",
+    tool: ['Flutter', 'React Native'],
+  ),
+  ServicesUtils(
+    name: 'UI/UX Designing',
+    icon: 'assets/icons/graphic.svg',
+    description:
+        "I'm creating elegant designs suited to your needs following core design theory. ",
+    tool: [
+      'FlutterFlow',
+      'Figma',
+    ],
+  ),
+  ServicesUtils(
+    name: 'Backend Development',
+    icon: 'assets/icons/database.svg',
+    description:
+        "I have knowledge in Firebase, Firebase Storage,Firestore,Databse and Beginner in Node.js & Express.js  ",
+    tool: ['FireBase', 'Node.js', 'Express.js', 'MySQL'],
+  ),
+  ServicesUtils(
+    name: 'Web Development',
+    icon: 'assets/icons/website.svg',
+    description:
+        "Do you have an idea for your next great website? Let's make it a reality.",
+    tool: [
+      'Flutter',
+      'Html,css,js',
+    ],
+  ),
+];
---
+++ lib/changes/links.dart
@@ -0,0 +1,40 @@
+// CV
+const String resume =
+    'https://drive.google.com/file/d/1GO1fsdz6nkPWZlZaGYTNKaofLCNhJDj6/view?usp=sharing';
+
+// Github Link
+const String gitHub = 'https://github.com/Himanshusha673';
+// Github Link
+const String facebook =
+    'https://www.facebook.com/profile.php?id=100075811569477';
+// Github Link
+const String twitter = 'https://twitter.com/msdhimanshu1';
+// Github Link
+const String youtube =
+    'https://www.youtube.com/channel/UCUvlFwi28OZ0IdtmvTYP0rw';
+// Github Link
+const String linkedin = 'https://www.linkedin.com/in/himanshu-sha-6b176a19b/';
+// Github Link
+const String buymeacoffee = 'https://www.buymeacoffee.com/himanshusaX';
+// Github Link
+const String instagram =
+    'https://www.instagram.com/k_vin_mitnick/?__coig_restricted=1';
+// socialLinks
+// const List<String> socialLinks = [
+//   "https://facebook.com/sudeshnb",
+//   // "https://instagram.com/sudesh_nish",
+//   "https://www.youtube.com/channel/UCXooUY2oL_eqGhTaZn-ExSg",
+//   "https://twitter.com/sudesh78",
+//   "https://linkedin.com/in/sudeshnb",
+//   "https://www.buymeacoffee.com/sudeshnb", "https://github.com/sudeshnb",
+// ];
+//
+const String profileImage =
+    "https://scontent-del1-1.xx.fbcdn.net/v/t39.30808-6/338926548_572354834873416_4333674219093567281_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=e3f864&_nc_ohc=JDtmPSrKwRQAX-oXdNB&_nc_ht=scontent-del1-1.xx&oh=00_AfDa-jQBA9uUSkxuqDEbWBlSGxOOgVftumF-MO02sHKyxQ&oe=64AD4DB6";
+//
+// Github Link
+const String fiverr = 'https://www.fiverr.com/himanshusaha844?up_rollout=true';
+// Github Link
+const String upwork = 'https://www.upwork.com/freelancers/~013d932d066de16fb7';
+// Github Link
+const String whatsapp = 'https://api.whatsapp.com/send?phone=9984619283';
